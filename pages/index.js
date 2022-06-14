44609e08489f8dc2bd58d88e8678db4051833d13
Siam456 <hasibul15-11038@diu.edu.bd>
2022-06-14 13:11:03 +0600

    get districdata from api

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,19 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import axios from "axios";
+import Head from "next/head";
+import Image from "next/image";
+import { useEffect } from "react";
+import styles from "../styles/Home.module.css";
+export const getServerSideProps = async () => {
+  const res = await axios.get(
+    "https://bdapis.herokuapp.com/api/v1.1/districts/"
+  );
 
-export default function Home() {
+  return {
+    props: { data: res.data.data },
+  };
+};
+
+export default function Home({ data }) {
   return (
     <div className={styles.container}>
       <Head>
@@ -17,38 +28,18 @@ export default function Home() {
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          <code className={styles.code}>Get all districts of Bangladesh</code>
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          {data.map((district) => {
+            return (
+              <div key={district._id} className={styles.card}>
+                <h2>District: {district.district} &rarr;</h2>
+                <p>Coordinates: {district.coordinates}</p>
+              </div>
+            );
+          })}
         </div>
       </main>
 
@@ -58,12 +49,12 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
     </div>
-  )
+  );
 }
